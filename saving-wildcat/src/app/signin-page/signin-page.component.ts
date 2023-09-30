a86169918b7c7e4d6374705248ae570ecf036b08
Alston Dias <Alston.Dias@ibm.com>
2023-09-30 12:38:35 +0530

    added route to the home-page

--- saving-wildcat/src/app/signin-page/signin-page.component.ts
+++ saving-wildcat/src/app/signin-page/signin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin-page',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninPageComponent {
 
+  constructor (private router : Router){ }
+
   username !: string
   password !: string
 
   ngOnInit():void{}
+
+  onSignIn(){
+    this.router.navigate(['home'])
+  }
 
 }
